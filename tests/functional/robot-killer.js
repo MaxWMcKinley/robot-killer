953c58c2736777e76b7c12c8764eff3ea82ab829
Max McKinley <maxwmckinley@gmail.com>
2017-06-09 16:32:06 -0500

    The test now iterates 100 times for each operating system and browser

--- tests/functional/robot-killer.js
+++ tests/functional/robot-killer.js
@@ -6,16 +6,18 @@ define(function (require) {
             name: 'robot-killer',
 
             'humanity checker': function () {
-                return this.remote
-                    .get(require.toUrl('robot-killer.html'))
-                    .setFindTimeout(5000)
-                    .findByTagName('button')
-                        .click()
-                        .end()
-                    .findByCssSelector("iframe[title='recaptcha challenge']")
-                    .then(function (iframe) {
-                        assert(iframe.isDisplayed(), 'iFrame should be displayed after bot clicks on submit');
-                    });
+               for(i=0; i<100; i++) {
+                    return this.remote
+                        .get(require.toUrl('robot-killer.html'))
+                        .setFindTimeout(5000)
+                        .findByTagName('button')
+                            .click()
+                            .end()
+                        .findByCssSelector("iframe[title='recaptcha challenge']")
+                        .then(function (iframe) {
+                            assert(iframe.isDisplayed(), 'iFrame should be displayed after bot clicks on submit');
+                        });
+                }
             }
         });
 	});
