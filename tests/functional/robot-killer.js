20b5c2009e114cd7a53d432c3ccdf30aae70e88d
Max McKinley <maxwmckinley@gmail.com>
2017-06-09 16:12:01 -0500

    Added testing to ensure proper robot killing

---
+++ tests/functional/robot-killer.js
@@ -0,0 +1,21 @@
+define(function (require) {
+	var registerSuite = require('intern!object');
+	var assert = require('intern/chai!assert');
+
+    registerSuite({
+            name: 'robot-killer',
+
+            'humanity checker': function () {
+                return this.remote
+                    .get(require.toUrl('robot-killer.html'))
+                    .setFindTimeout(5000)
+                    .findByTagName('button')
+                        .click()
+                        .end()
+                    .findByCssSelector("iframe[title='recaptcha challenge']")
+                    .then(function (iframe) {
+                        assert(iframe.isDisplayed(), 'iFrame should be displayed after bot clicks on submit');
+                    });
+            }
+        });
+	});
